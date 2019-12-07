67bc34ea87af216840bf4cc04de774d7f55ff36b
keshav.lodhi <keshav.lodhi@widas.in>
2019-12-07 21:25:56 +0530

    Scala programming OOBasics.

--- src/main/scala/tour/of/scala/method/notations/MethodNotations.scala
+++ src/main/scala/tour/of/scala/method/notations/MethodNotations.scala
@@ -2,24 +2,6 @@ package tour.of.scala.method.notations
 
 object MethodNotations extends App {
 
-  val mary = new Person("Mary", "Inception")
-  /** *****************************"operators" in Scala **********************************/
-  val tom = new Person("Tom", "Fight Club")
-  println(mary.likes("Inception")) // Both are equivalent
-  /** ***************************infix notation in Scala *********************************/
-  println(mary likes "Inception") // infix notation or operator notation (is also called syntactic sugar): Are applicable if we have single parameter
-  /** *********************************prefix in Scala **********************************/
-  val x = -1 // Equivalent with 1.unary_-
-  //println(mary hangOutWith tom)
-  println(mary + tom) // Replacing "hangOutWith" with "+" is still a valid method
-  println(mary.+(tom)) // Both are equivalent
-
-  // ALL OPERATORS ARE METHODS
-  // Akka actors have !(tell) and ?(ask) operator as method
-  println(1 + 2)
-  println(1.+(2))
-  val y = 1.unary_-
-
   class Person(val name: String, favoriteMovie: String) {
     def likes(movie: String): Boolean = movie == favoriteMovie
 
@@ -33,17 +15,44 @@ object MethodNotations extends App {
     def apply(): String = s"Hi, my name is $name and i like $favoriteMovie"
   }
 
+  val mary = new Person("Mary", "Inception")
+  println(mary.likes("Inception")) // Both are equivalent
+  /*****************************infix notation in Scala *********************************/
+  println(mary likes "Inception") // infix notation or operator notation (is also called syntactic sugar): Are applicable if we have single parameter
+
+  /*******************************"operators" in Scala **********************************/
+  val tom = new Person("Tom", "Fight Club")
+
+  //println(mary hangOutWith tom)
+  println(mary + tom) // Replacing "hangOutWith" with "+" is still a valid method
+  println(mary.+(tom)) // Both are equivalent
+
+  // ALL OPERATORS ARE METHODS
+  // Akka actors have !(tell) and ?(ask) operator as method
+  println(1 + 2)
+  println(1.+(2))
+
+  /***********************************prefix in Scala **********************************/
+  val x = -1 // Equivalent with 1.unary_-
+  val y = 1.unary_-
   // unary_ prefix only works with +, -, ~, !
 
   println(!mary)
   println(mary.unary_!)
 
-  /** *********************************postfix in Scala **********************************/
+  /***********************************postfix in Scala **********************************/
   println(mary.isAlive)
   println(mary isAlive) // postfix operator notation is only available to methods without parameter
 
-  /** *********************************apply method in Scala **********************************/
+  /***********************************apply method in Scala **********************************/
   println(mary.apply)
   println(mary()) // Both are equivalent
+
+  /*
+    1. Overload the + operator
+       marry + "the rockstar" => new person "Merry (the rockstar)"
+   */
+
+
 
 }
